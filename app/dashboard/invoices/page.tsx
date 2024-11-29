29bee8b954fc22b37a95d1f20fb123bcd1d7d736
geekyharsh05 <hp424805@gmail.com>
2024-11-29 22:48:49 +0530

    feat: Add InvoicesRoute component to display invoices with create option

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,32 @@
+import InvoiceList from "@/app/components/InvoiceList";
+import { buttonVariants } from "@/components/ui/button";
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { PlusIcon } from "lucide-react";
+import Link from "next/link";
+
+export default function InvoicesRoute() {
+    return (
+        <Card>
+            <CardHeader>
+                <div className="flex items-center justify-between">
+                    <div>
+                        <CardTitle className="text-2xl font-bold">Invoices</CardTitle>
+                        <CardDescription>View all your invoices.</CardDescription>
+                    </div>
+
+                    <div>
+                        <Link href="/dashboard/invoices/create" className={buttonVariants({
+                            className: "w-full",
+                            variant: "outline",
+                        })}> 
+                            <PlusIcon /> Create Invoice
+                        </Link>
+                    </div>
+                </div>
+            </CardHeader>
+            <CardContent>
+                <InvoiceList />
+            </CardContent>
+        </Card>
+    )
+}
