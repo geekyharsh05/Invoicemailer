127df18e34f8695e23453e575e59b1c35918518e
geekyharsh05 <hp424805@gmail.com>
2024-11-29 22:49:12 +0530

    feat: Add Onboarding component for user detail submission

---
+++ app/onboarding/page.tsx
@@ -0,0 +1,79 @@
+"use client"
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import SubmitButton from "../components/SubmitButtons";
+import { useActionState } from "react";
+import { onBoardUser } from "@/lib/actions/actions";
+import { useForm } from "@conform-to/react"
+import { parseWithZod } from "@conform-to/zod";
+import { onboardingSchema } from "../utils/zod-schemas";
+
+export default function Onboarding() {
+    const [lastResult, action] = useActionState(onBoardUser, undefined);
+    const [form, fields] = useForm({
+        lastResult,
+
+        onValidate({ formData }) {
+            return parseWithZod(formData, {
+                schema: onboardingSchema,
+            })
+        },
+
+        shouldValidate: "onBlur",
+        shouldRevalidate: "onInput"
+    })
+
+    return (
+        <div className="min-h-screen w-screen flex items-center justify-center">
+            <Card className="max-w-sm mx-auto">
+                <CardHeader>
+                    <CardTitle className="text-xl">
+                        You are almost finished!
+                    </CardTitle>
+                    <CardDescription>Enter your details to get started.</CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <form className="grid gap-4" action={action} id={form.id} onSubmit={form.onSubmit} noValidate>
+                        <div className="grid grid-cols-2 gap-4">
+                            <div className="grid gap-2">
+                                <Label>First Name</Label>
+                                <Input
+                                    name={fields.firstName.name}
+                                    key={fields.firstName.key}
+                                    defaultValue={fields.firstName.initialValue}
+                                    type="text"
+                                    placeholder="Graeme"
+                                />
+                                <p className="text-red-500 text-sm">{fields.firstName.errors}</p>
+                            </div>
+                            <div className="grid gap-2">
+                                <Label>Last Name</Label>
+                                <Input
+                                    name={fields.lastName.name}
+                                    key={fields.lastName.key}
+                                    defaultValue={fields.lastName.initialValue}
+                                    type="text"
+                                    placeholder="Smith"
+                                />
+                                <p className="text-red-500 text-sm">{fields.lastName.errors}</p>
+                            </div>
+                        </div>
+                        <div className="grid gap-2">
+                            <Label>Address</Label>
+                            <Input
+                                name={fields.address.name}
+                                key={fields.address.key}
+                                defaultValue={fields.address.initialValue}
+                                type="text"
+                                placeholder="The Wanderers, Johannesburg"
+                            />
+                            <p className="text-red-500 text-sm">{fields.address.errors}</p>
+                        </div>
+                        <SubmitButton text="Finish Onboarding" />
+                    </form>
+                </CardContent>
+            </Card>
+        </div>
+    )
+}
