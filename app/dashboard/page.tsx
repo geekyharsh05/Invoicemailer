70089211db6ea0b80d553ccd626a063e290d20f6
geekyharsh05 <hp424805@gmail.com>
2024-11-29 08:47:08 +0530

    feat: Remove sign out functionality from dashboard and add Sheet component for UI

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,21 +1,9 @@
-import { Button } from "@/components/ui/button"
-import { signOut } from "../utils/auth"
-import { requireUser } from "../utils/hooks"
+
 
 export default async function DashboardRoute() {
-    const session = await requireUser()
-
     return (
         <div className="flex h-screen w-full items-center justify-center px-4">
             <h1 className="text-3xl">Dashboard</h1>
-            <form
-                action={async () => {
-                    "use server"
-                    await signOut()
-                }}
-            >
-                <Button type="submit" className="mt-4">Sign Out</Button>
-            </form>
         </div>
     )
 }
