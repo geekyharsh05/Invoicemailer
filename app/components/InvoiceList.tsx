9228113807730a1c0327190c5c1219d24e6b6eb4
geekyharsh05 <hp424805@gmail.com>
2024-12-01 01:08:25 +0530

    feat: Integrate EmptyState component in InvoiceList for improved user experience when no invoices are found

--- app/components/InvoiceList.tsx
+++ app/components/InvoiceList.tsx
@@ -5,6 +5,7 @@ import prisma from "../utils/db";
 import { requireUser } from "@/hooks/require-user";
 import { formatCurrency } from "@/hooks/format-currency";
 import { Badge } from "@/components/ui/badge";
+import EmptyState from "./EmptyState";
 
 async function fetchInvoices(userId: string) {
     const data = await prisma.invoice.findMany({
@@ -32,38 +33,49 @@ async function InvoiceList() {
     const data = await fetchInvoices(session.user?.id as string);
 
     return (
-        <Table>
-            <TableHeader>
-                <TableRow>
-                    <TableHead>Invoice ID</TableHead>
-                    <TableHead>Customer</TableHead>
-                    <TableHead>Amount</TableHead>
-                    <TableHead>Status</TableHead>
-                    <TableHead>Date</TableHead>
-                    <TableHead className="text-right">Actions</TableHead>
-                </TableRow>
-            </TableHeader>
-            <TableBody>
-                {data.map((invoice) => (
-                    <TableRow key={invoice.id}>
-                        <TableCell>{invoice.invoiceNumber}</TableCell>
-                        <TableCell>{invoice.clientName}</TableCell>
-                        <TableCell>{formatCurrency({
-                            amount: invoice.total,
-                            currency: invoice.currency as any,
-                        })}</TableCell>
-                        <TableCell>
-                            <Badge>{invoice.status}</Badge>
-                        </TableCell>
-                        <TableCell>{new Intl.DateTimeFormat("en-US", {
-                            dateStyle: "medium",
-                        }).format(invoice.date)}
-                        </TableCell>
-                        <TableCell className="text-right"><InvoiceActions id={invoice.id} status={invoice.status} /></TableCell>
-                    </TableRow>
-                ))}
-            </TableBody>
-        </Table>
+        <>
+            {data.length === 0 ? (
+                <EmptyState
+                    title="No invoices found"
+                    description="Create an invoice to get started."
+                    buttontext="Create Invoice"
+                    href="/dashboard/invoices/create"
+                />
+            ) : (
+                <Table>
+                    <TableHeader>
+                        <TableRow>
+                            <TableHead>Invoice ID</TableHead>
+                            <TableHead>Customer</TableHead>
+                            <TableHead>Amount</TableHead>
+                            <TableHead>Status</TableHead>
+                            <TableHead>Date</TableHead>
+                            <TableHead className="text-right">Actions</TableHead>
+                        </TableRow>
+                    </TableHeader>
+                    <TableBody>
+                        {data.map((invoice) => (
+                            <TableRow key={invoice.id}>
+                                <TableCell>{invoice.invoiceNumber}</TableCell>
+                                <TableCell>{invoice.clientName}</TableCell>
+                                <TableCell>{formatCurrency({
+                                    amount: invoice.total,
+                                    currency: invoice.currency as any,
+                                })}</TableCell>
+                                <TableCell>
+                                    <Badge>{invoice.status}</Badge>
+                                </TableCell>
+                                <TableCell>{new Intl.DateTimeFormat("en-US", {
+                                    dateStyle: "medium",
+                                }).format(invoice.date)}
+                                </TableCell>
+                                <TableCell className="text-right"><InvoiceActions id={invoice.id} status={invoice.status} /></TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            )}
+        </>
     )
 }
 
