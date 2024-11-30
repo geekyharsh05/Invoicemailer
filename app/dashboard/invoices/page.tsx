da7ba595b4d88e3b9ef27282ebdfb8a4fe2f64d6
geekyharsh05 <hp424805@gmail.com>
2024-12-01 01:09:08 +0530

    feat: Enhance Invoices page with improved description and loading state using Suspense and Skeleton

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,8 +1,10 @@
 import InvoiceList from "@/app/components/InvoiceList";
 import { buttonVariants } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import { Skeleton } from "@/components/ui/skeleton";
 import { PlusIcon } from "lucide-react";
 import Link from "next/link";
+import { Suspense } from "react";
 
 export default function InvoicesRoute() {
     return (
@@ -11,21 +13,23 @@ export default function InvoicesRoute() {
                 <div className="flex items-center justify-between">
                     <div>
                         <CardTitle className="text-2xl font-bold">Invoices</CardTitle>
-                        <CardDescription>View all your invoices.</CardDescription>
+                        <CardDescription>Manage and view all your invoices in one place.</CardDescription>
                     </div>
 
                     <div>
                         <Link href="/dashboard/invoices/create" className={buttonVariants({
                             className: "w-full",
                             variant: "outline",
-                        })}> 
+                        })}>
                             <PlusIcon /> Create Invoice
                         </Link>
                     </div>
                 </div>
             </CardHeader>
             <CardContent>
-                <InvoiceList />
+                <Suspense fallback={<Skeleton className="w-full h-[500px]" />}>
+                    <InvoiceList />
+                </Suspense>
             </CardContent>
         </Card>
     )
