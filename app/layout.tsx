7e040e109f84779ecd44d9f20ee718d0f06e0ae6
geekyharsh05 <hp424805@gmail.com>
2024-12-01 23:35:57 +0530

    fix: replace local fonts with Google Poppins font and add react-dnd dependency

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,13 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+const poppins = Poppins({
+  weight: ["400", "900", "700", "500"],
+  display: "swap",
+  subsets: ["latin"],
+  variable: "--poppins-font",
 });
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
-
 
 export const metadata: Metadata = {
   title: "Invoice Mailer",
@@ -27,7 +22,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable}`}
+        className={poppins.className}
       >
         {children}
       </body>
